1a2513d6d830cc62ce5540ef421e2bb53101c919
RAYTRAC3R <samipiplup@gmail.com>
2026-05-13 16:05:38 -0500

    Update settings.py

--- animYtDB/settings.py
+++ animYtDB/settings.py
@@ -84,11 +84,13 @@ WSGI_APPLICATION = "animYtDB.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    ),
 }
 
 
@@ -139,10 +141,3 @@ MEDIA_ROOT = "/media/"
 MEDIA_URL = "/media/"
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
-
-import dj_database_url
-
-DATABASES['default'] = dj_database_url.config(
-    conn_max_age=600,
-    conn_health_checks=True,
-)
