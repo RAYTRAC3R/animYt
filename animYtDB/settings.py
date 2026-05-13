06414d9802aea3e47f138062e37d31cffe0b6c53
RAYTRAC3R <samipiplup@gmail.com>
2026-05-13 16:03:17 -0500

    postgres

--- animYtDB/settings.py
+++ animYtDB/settings.py
@@ -139,3 +139,10 @@ MEDIA_ROOT = "/media/"
 MEDIA_URL = "/media/"
 
 DATABASE_URL = os.environ.get("DATABASE_URL")
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)
