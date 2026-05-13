cfcc3ddbdfa616058ae5db2a7070cc0ae7e533fa
RAYTRAC3R <samipiplup@gmail.com>
2026-05-13 14:22:09 -0500

    Update urls.py

--- entries/urls.py
+++ entries/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", views.index, name="index"),
     # ex: /entry/5/
     path('entry/<int:entry_id>/', views.entry, name='entry'),
     # ex: /creator/5/
