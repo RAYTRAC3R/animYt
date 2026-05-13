a9676a0d316ccfc061b35d737203a160bba71612
RAYTRAC3R <samipiplup@gmail.com>
2026-05-13 12:12:50 -0500

    fly io fixes

--- animYtDB/settings.py
+++ animYtDB/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ ]
+APP_NAME = os.environ.get("FLY_APP_NAME")
+ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
 
 
 # Application definition
