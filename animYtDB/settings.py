75a03e47433311618342d1cae16b7a513d996748
RAYTRAC3R <samipiplup@gmail.com>
2026-05-14 14:38:22 -0500

    Update settings.py

--- animYtDB/settings.py
+++ animYtDB/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["ANIMYT_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ ]
+ALLOWED_HOSTS = [ raytrac3r.pythonanywhere.com ]
 
 
 # Application definition
